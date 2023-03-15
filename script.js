5ffa87709ea2b831a506d57de3ff8049a20623a4
Lena <lena.sterz98@gmail.com>
2023-03-15 16:45:26 +0100

    Add remove todo function, localStorage function and import local storage function

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 const input = document.getElementById("input");
 const submitBtn = document.getElementById("submit");
 const unfinishedList = document.getElementById("unfinished-list");
+const finishedList = document.getElementById("finished-list");
+const deleteBtn = document.querySelectorAll("button:has(i.fa-trash)");
 
 // Functions
+
+// Add the item to the DOM
 function addItem(e) {
 	e.preventDefault();
 
-	const todoText = input.value;
+	const inputText = input.value;
 
-	if (todoText === "") {
+	if (inputText === "") {
 		alert("Please enter something");
 	} else {
-		createTodo(todoText);
+		createTodo(inputText);
+		addToStorage(inputText);
 	}
 }
 
-function createTodo(todoText) {
+// Create the item
+function createTodo(inputText) {
 	// Create the elements
 	const li = document.createElement("li");
 	const span = document.createElement("span");
 	const div = document.createElement("div");
-	const checkBtn = createButton("fa-solid fa-check");
-	const trashBtn = createButton("fa-solid fa-trash");
+	const checkBtn = createButton("fa-solid fa-check", "check");
+	const trashBtn = createButton("fa-solid fa-trash", "delete");
 
 	// Set the text of the todo item
-	span.textContent = todoText;
+	span.textContent = inputText;
 
 	// Append the elements to the list
 	div.appendChild(checkBtn);
@@ -34,12 +40,13 @@ function createTodo(todoText) {
 
 	unfinishedList.appendChild(li);
 
-	// Reset todoText value
+	// Reset inputText value
 	input.value = "";
 }
 
-function createButton(classes) {
+function createButton(classes, btnType) {
 	const button = document.createElement("button");
+	button.className = btnType;
 	const icon = createIcon(classes);
 	button.appendChild(icon);
 	return button;
@@ -51,5 +58,35 @@ function createIcon(iconClass) {
 	return icon;
 }
 
+// Add the todo item to local storage
+function addToStorage(inputText) {
+	const itemsFromStorage = getItemsFromStorage();
+
+	itemsFromStorage.push(inputText);
+
+	localStorage.setItem("todos", JSON.stringify(itemsFromStorage));
+}
+
+// Get the local storage and parse it into an array
+function getItemsFromStorage() {
+	let itemsFromStorage;
+	if (localStorage.getItem("todos") === null) {
+		itemsFromStorage = [];
+	} else {
+		itemsFromStorage = JSON.parse(localStorage.getItem("todos"));
+	}
+
+	return itemsFromStorage;
+}
+
+// Remove the item from the dom
+function changeTodo(e) {
+	if (e.target.classList.contains("fa-trash")) {
+		e.target.parentElement.parentElement.parentElement.remove();
+	}
+}
+
 // Event Listeners
 submitBtn.addEventListener("click", addItem);
+unfinishedList.addEventListener("click", changeTodo);
+finishedList.addEventListener("click", changeTodo);
